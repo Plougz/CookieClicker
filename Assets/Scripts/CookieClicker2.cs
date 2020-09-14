17a11e499feac3a24c7c91c5249fff01569da6ff
Rasmus Ploug <rasmusploug@live.dk>
2020-09-14 10:47:26 +0200

    Indsættelse af cookieclicker
    Første indsættelse af koden :)

---
+++ Assets/Scripts/CookieClicker2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CookieClicker2 : MonoBehaviour {
+
+
+    bool click;
+    public int counter;
+    public Text scoreText;
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        click = Input.GetKeyDown(KeyCode.C);
+        if (click == true) {
+            counter += 1;
+                }
+        scoreText.text = "Popped Corn: " + Convert.ToString(counter);
+    }
+        }
